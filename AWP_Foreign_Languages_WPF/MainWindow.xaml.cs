3de3900caf8713b66e0045ef594e88d618434e85
hjghjghj <hjghjghjghj@ghjghj.ghgh>
2022-06-13 19:27:20 +0500

    ,

--- AWP_Foreign_Languages_WPF/MainWindow.xaml.cs
+++ AWP_Foreign_Languages_WPF/MainWindow.xaml.cs
@@ -22,14 +22,14 @@ namespace AWP_Foreign_Languages_WPF
             InitializeComponent();
 
             string welcomeMessage = @"
-Данное приложение находится на стадии разработки, так что возможны баги, вылеты и иные проблемы.
+Данное приложение находится на стадии разработки и тестирования, так что возможны баги, вылеты и иные проблемы.
 
 Обо всех найденых ошибках сообщайте по электронному адресу: Kat89826420232@gmail.com.
 
 Благодарим за пользование нашей программы!
 ";
 
-            // MessageBox.Show(welcomeMessage,"Добро пожаловать!", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show(welcomeMessage,"Добро пожаловать!", MessageBoxButton.OK, MessageBoxImage.Information);
 
             App.MF = MainFrame;
             App.PersonalPageButton = ButtonPersonalPage;
